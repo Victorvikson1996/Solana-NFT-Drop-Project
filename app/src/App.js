e2fe5c1b2c4a973a18eb08f41e6ee2768b354d37
Ezea Victor Chukwuebuka <victorvikson@gmail.com>
2021-12-09 01:14:39 +0530

    initial Commit

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,80 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'victorebukaezea';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const checkIfWallectisConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log('Phantom wallet found!');
+
+
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log(
+            'Connected with Public Key:',
+            response.publicKey.toString()
+          )
+        }
+      } else {
+        alert('solana object not found! Get a Phantom Wallet 👻')
+      }
+
+
+
+    } catch (error) {
+      console.error(error)
+
+    }
+  };
+
+  /*
+  * Let's define this method so our code doesn't break.
+  * We will write the logic for this next!
+  */
+
+  const connectWallet = async () => {
+
+  }
+
+  const renderNotConnectedContainer = () => {
+    <button className='cta-button connect-wallet-button' onClick={connectWallet}>
+      Connect to Wallet
+    </button>
+  }
+
+
+
+
+
+  /*
+  * When our component first mounts, let's check to see if we have a connected
+  * Phantom Wallet
+  */
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWallectisConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load, onLoad')
+  }, [])
+
+
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
+          {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
